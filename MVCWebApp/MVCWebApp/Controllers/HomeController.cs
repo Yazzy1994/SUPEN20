76de5fc7145fc685050b3f937748ee6782615b5a
Yasnia Cruz <yasnia.cruz@integrationsbolaget.se>
2020-03-02 14:05:46 +0100

    vet int

--- MVCWebApp/MVCWebApp/Controllers/HomeController.cs
+++ MVCWebApp/MVCWebApp/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVCWebApp.Models;
-using SystemAPI.Models;
+
 
 namespace MVCWebApp.Controllers
 {
