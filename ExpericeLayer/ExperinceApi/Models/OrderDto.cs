68a0ed9e0ce58e5f982070e2a201913363b8a969
Alicia Grevsten <alicia.grevsten@gmail.com>
2020-03-05 11:32:45 +0100

    Skapat Experience-api + småändringar

---
+++ ExpericeLayer/ExperinceApi/Models/OrderDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExperinceApi.Models
+{
+    public class OrderDto
+    {
+        public Guid OrderId { get; set; }
+        public int OrderNumber { get; set; }
+        public ICollection<OrderItemDto> OrderItems { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public string CustomerId { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        Approved,
+        Denied
+    }
+}
